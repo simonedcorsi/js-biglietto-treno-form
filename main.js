44746396f30356cc93120546da60c10680e30f9f
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-20 16:51:06 +0100

    es

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Chiedo all'utente la distanza da percorrere e l'età
-let kmtreno = parseInt(prompt("Quanti Km desideri percorerre?"));
-let eta = parseInt(prompt("Quanti anni hai?"));
+let kmtreno = ("Quanti Km desideri percorerre?");
+let eta = ("Quanti anni hai?");
 
 // Calcolo il prezzo di base (kilometri* tariffa al kilometro)
 let price = kmtreno * 0.21;
@@ -22,3 +22,30 @@ if(eta > 65){
 
 // Prezzo finale biglietto
 console.log('Il tuo biglietto costa: '+price+' €');
+
+
+
+
+// Creare evento al click del bottone Genera
+const name = document.getElementById('name');
+const km = document.getElementById('km');
+const age = document.getElementById('age');
+
+document.getElementById("click").addEventListener("click", function() {
+     
+// Inserire nel html nome e cognome passeggero
+name.innerHTML = name
+
+// Chiedere età passeggero
+
+
+// inserire nel html costo biglietto 
+
+
+// Se l'età è minore di 18 applico uno sconto del 20%
+
+
+// Se l'età è maggiore di 65 applico uno sconto del 40%
+
+
+} ) ;
